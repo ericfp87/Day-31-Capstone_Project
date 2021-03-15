190a108b181169f1d948bd3cfebc8866d7977648
ericfp87 <52512077+ericfp87@users.noreply.github.com>
2021-03-15 16:38:18 -0300

    Final

--- main.py
+++ main.py
@@ -2,25 +2,27 @@ from tkinter import *
 import pandas
 import random
 
-
-
-
 BACKGROUND_COLOR = "#B1DDC6"
-
-data = pandas.read_csv("data/french_words.csv")
-to_learn = data.to_dict(orient="records")
+to_learn = {}
 current_card = {}
-#Flip the Cards!
 
+try:
+    data = pandas.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pandas.read_csv("data/french_words.csv")
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
+
+
+# Flip the Cards!
 def flip_cards():
-
     canvas.itemconfig(card_background, image=card_back_img)
     canvas.itemconfig(title_text, text="English", fill="white")
     canvas.itemconfig(word_text, text=current_card["English"], fill="white")
 
 
-
-#New Flash Cards
+# New Flash Cards
 def next_card():
     global current_card, flip_timer
     window.after_cancel(flip_timer)
@@ -30,6 +32,12 @@ def next_card():
     canvas.itemconfig(card_background, image=front_img)
     flip_timer = window.after(3000, flip_cards)
 
+
+def is_known():
+    to_learn.remove(current_card)
+    data = pandas.DataFrame(to_learn)
+    data.to_csv("data/Words_to_learn.csv", index=False)
+    next_card()
 
 
 # UI
@@ -47,22 +55,17 @@ card_back_img = PhotoImage(file="images/card_back.png")
 canvas.grid(row=0, column=0, columnspan=2)
 
 # Labels
-
 title_text = canvas.create_text(400, 150, font=("Arial", 40, "italic"), text="Title")
 word_text = canvas.create_text(400, 263, font=("Arial", 60, "bold"), text="Word")
 
-
 # Buttons
-
 right_image = PhotoImage(file=".\images\Right.png")
-right_button = Button(image=right_image, highlightthickness=0, command=next_card)
+right_button = Button(image=right_image, highlightthickness=0, command=is_known)
 right_button.grid(row=1, column=1)
 
 wrong_image = PhotoImage(file=".\images\wrong.png")
 wrong_button = Button(image=wrong_image, highlightthickness=0, command=next_card)
 wrong_button.grid(row=1, column=0)
-
-
 
 next_card()
 
